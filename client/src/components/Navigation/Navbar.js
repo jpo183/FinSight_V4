9c6b275bb7f608c1f2517d78247a6e5ae6f207d7
Jonathon Pocius <jonathonpocius@Jonathons-MacBook-Pro.local>
2025-02-25 23:35:08 -0500

    update nav bar

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box, Menu, MenuItem } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [salesMenu, setSalesMenu] = React.useState(null);
+
+  const handleSalesMenuOpen = (event) => {
+    setSalesMenu(event.currentTarget);
+  };
+
+  const handleSalesMenuClose = () => {
+    setSalesMenu(null);
+  };
+
+  const navigateToSalesPage = (path) => {
+    navigate(path);
+    handleSalesMenuClose();
+  };
 
   return (
     <AppBar position="static">
@@ -15,6 +30,26 @@ const Navbar = () => {
           <Button color="inherit" onClick={() => navigate('/dashboard')}>
             Dashboard
           </Button>
+          
+          {/* Sales dropdown menu */}
+          <Button 
+            color="inherit" 
+            onClick={handleSalesMenuOpen}
+            endIcon={<ArrowDropDownIcon />}
+          >
+            Sales
+          </Button>
+          <Menu
+            anchorEl={salesMenu}
+            open={Boolean(salesMenu)}
+            onClose={handleSalesMenuClose}
+          >
+            <MenuItem onClick={() => navigateToSalesPage('/sales')}>Sales Home</MenuItem>
+            <MenuItem onClick={() => navigateToSalesPage('/sales/dashboard')}>Sales Dashboard</MenuItem>
+            <MenuItem onClick={() => navigateToSalesPage('/sales/analytics')}>Sales Analytics</MenuItem>
+            <MenuItem onClick={() => navigateToSalesPage('/sales/kpi/manage')}>Manage KPIs</MenuItem>
+          </Menu>
+          
           <Button color="inherit" onClick={() => navigate('/config')}>
             Config
           </Button>
